e9ce2a571b352590911ace8030a7752b821ee7d6
YTeckk <163964829+YTeckk@users.noreply.github.com>
2024-04-20 14:53:45 +0800

    added community page and create new post page

--- app/src/main/java/my/edu/utar/assignment2/CommunityPage/CommunityActivity.java
+++ app/src/main/java/my/edu/utar/assignment2/CommunityPage/CommunityActivity.java
@@ -1,26 +1,52 @@
 package my.edu.utar.assignment2.CommunityPage;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import my.edu.utar.assignment2.CommunityPage.DatabaseHelper;
 import my.edu.utar.assignment2.HomePage;
 import my.edu.utar.assignment2.R;
 
+
 public class CommunityActivity extends AppCompatActivity {
+
+    private DatabaseHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_community);
 
+        dbHelper = new DatabaseHelper(this);
+
+        // Set onClickListener for the "Community" TextView
+        TextView communityTextView = findViewById(R.id.C_textView2);
+        communityTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Retrieve data from the database
+                String postData = retrieveDataFromDatabase();
+
+                // Display retrieved data
+                Toast.makeText(CommunityActivity.this, postData, Toast.LENGTH_SHORT).show();
+            }
+        });
+
         //back button
         ImageView BackBtn = findViewById(R.id.C_backBtn);
-
         BackBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -28,13 +54,38 @@ public class CommunityActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        //Create post
+
+        // Create post
         ConstraintLayout createPostLayout = findViewById(R.id.CreatePost);
         createPostLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Intent intent = new Intent(CommunityActivity.this, CreateNewPost.class);
-//                startActivity(intent);            }
+                Intent intent = new Intent(CommunityActivity.this, CreateNewPost.class);
+                startActivity(intent);
+            }
         });
+    }
+
+    private String retrieveDataFromDatabase() {
+        String result = "";
+
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        Cursor cursor = db.query(dbHelper.getTableName(), null, null, null, null, null, DatabaseHelper.getColumnTimestamp() + " DESC");
+
+        if (cursor != null) {
+            while (cursor.moveToNext()) {
+                @SuppressLint("Range") String username = cursor.getString(cursor.getColumnIndex(DatabaseHelper.getColumnUsername()));
+                @SuppressLint("Range") String datetime = cursor.getString(cursor.getColumnIndex(DatabaseHelper.getColumnTimestamp()));
+                @SuppressLint("Range") String content = cursor.getString(cursor.getColumnIndex(DatabaseHelper.getColumnContent()));
+
+                result += "Username: " + username + "\n"
+                        + "Date & Time: " + datetime + "\n"
+                        + "Content: " + content + "\n\n";
+            }
+            cursor.close();
+        }
+        db.close();
+
+        return result;
     }
 }
---
+++ app/src/main/java/my/edu/utar/assignment2/CommunityPage/CreateNewPost.java
@@ -0,0 +1,127 @@
+package my.edu.utar.assignment2.CommunityPage;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import my.edu.utar.assignment2.R;
+
+
+public class CreateNewPost extends AppCompatActivity {
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private static final int STORAGE_PERMISSION_CODE = 101;
+
+
+    private ImageView uploadedImageView;
+    private Button uploadImageButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_new_post);
+
+        //back button
+        ImageView BackBtn = findViewById(R.id.backBtn);
+
+        BackBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CreateNewPost.this, CommunityActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        uploadedImageView = findViewById(R.id.uploadedImageView);
+        uploadImageButton = findViewById(R.id.uploadImageButton);
+
+        uploadImageButton.setOnClickListener(v -> {
+            // Check if permission is granted
+            if (ContextCompat.checkSelfPermission(CreateNewPost.this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                // Permission is already granted
+                openImagePicker();
+            } else {
+                // Permission is not granted, request it
+                ActivityCompat.requestPermissions(CreateNewPost.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+            }
+        });
+
+        EditText postContentEditText = findViewById(R.id.postContentEditText);
+        //post button
+        Button postButton = findViewById(R.id.postButton);
+
+        postButton.setOnClickListener(v -> {
+            // Get post content, username, and image
+            String postContent = postContentEditText.getText().toString();
+            String username = "User";
+            byte[] imageByteArray = getImageByteArray();
+
+            // Insert post data into the database
+            DatabaseHelper dbHelper = new DatabaseHelper(CreateNewPost.this);
+            dbHelper.insertPost(username, postContent, imageByteArray);
+
+            // Display toast message
+            Toast.makeText(CreateNewPost.this, "Post successfully", Toast.LENGTH_SHORT).show();
+
+            // Navigate back to the Community page
+            Intent intent = new Intent(CreateNewPost.this, CommunityActivity.class);
+            startActivity(intent);
+        });
+    }
+
+    private byte[] getImageByteArray() {
+        Bitmap bitmap = ((BitmapDrawable) uploadedImageView.getDrawable()).getBitmap();
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        return stream.toByteArray();
+    }
+
+    private void openImagePicker() {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(galleryIntent, PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == STORAGE_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted
+                openImagePicker();
+            } else {
+                Toast.makeText(this, "Permission Denied!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri imageUri = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
+                uploadedImageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
