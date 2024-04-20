2ad842bf5694ecc8c6dff0e55fa141afab848b3a
QIAOLIN321 <122597455+QIAOLIN321@users.noreply.github.com>
2024-04-20 16:13:17 +0800

    Set Margin

--- app/src/main/java/my/edu/utar/assignment2/ProfilePage/Profile.java
+++ app/src/main/java/my/edu/utar/assignment2/ProfilePage/Profile.java
@@ -33,6 +33,7 @@ import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
@@ -55,6 +56,8 @@ import java.util.Map;
 import java.util.Objects;
 
 import de.hdodenhof.circleimageview.CircleImageView;
+import my.edu.utar.assignment2.CommunityPage.CommunityActivity;
+import my.edu.utar.assignment2.CommunityPage.CreateNewPost;
 import my.edu.utar.assignment2.HomePage;
 import my.edu.utar.assignment2.LearningPage.LearningPage;
 import my.edu.utar.assignment2.R;
@@ -176,7 +179,8 @@ public class Profile extends AppCompatActivity {
         chatBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(Profile.this, "hi", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(Profile.this, CreateNewPost.class);
+                startActivity(intent);
             }
         });
 
@@ -202,42 +206,49 @@ public class Profile extends AppCompatActivity {
     }
 
     private void loadUserProfile() {
-        db.collection("users").document(auth.getCurrentUser().getUid())
-                .get()
-                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-                    @Override
-                    public void onSuccess(DocumentSnapshot documentSnapshot) {
-                        if (documentSnapshot.exists()) {
-                            String name = documentSnapshot.getString("username");
-                            String email = documentSnapshot.getString("email");
-                            String profileImageUrl = documentSnapshot.getString("profileImageUrl");
-                            String location = documentSnapshot.getString("location");
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentUser != null) {
+            // User is signed in, load user profile
+            String userId = currentUser.getUid();
+            // Load user profile using userId
+        } else {
+            db.collection("users").document(auth.getCurrentUser().getUid())
+                    .get()
+                    .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                        @Override
+                        public void onSuccess(DocumentSnapshot documentSnapshot) {
+                            if (documentSnapshot.exists()) {
+                                String name = documentSnapshot.getString("username");
+                                String email = documentSnapshot.getString("email");
+                                String profileImageUrl = documentSnapshot.getString("profileImageUrl");
+                                String location = documentSnapshot.getString("location");
 
-                            nameText.setText(name);
-                            emailText.setText(email);
+                                nameText.setText(name);
+                                emailText.setText(email);
 
-                            // Check if location exists, if not, set a default value
-                            if (location == null || location.isEmpty()) {
-                                location = "Unknown";
-                            }
-                            locationText.setText(location);
+                                // Check if location exists, if not, set a default value
+                                if (location == null || location.isEmpty()) {
+                                    location = "Unknown";
+                                }
+                                locationText.setText(location);
 
-                            if (profileImageUrl != null && !profileImageUrl.isEmpty()) {
-                                Glide.with(Profile.this).load(profileImageUrl).into(avatarImage);
+                                if (profileImageUrl != null && !profileImageUrl.isEmpty()) {
+                                    Glide.with(Profile.this).load(profileImageUrl).into(avatarImage);
+                                } else {
+                                    Glide.with(Profile.this).load(R.drawable.profile).into(avatarImage);
+                                }
                             } else {
-                                Glide.with(Profile.this).load(R.drawable.profile).into(avatarImage);
+                                Log.d("ProfileActivity", "No such document");
                             }
-                        } else {
-                            Log.d("ProfileActivity", "No such document");
                         }
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.e("ProfileActivity", "Error getting document", e);
-                    }
-                });
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.e("ProfileActivity", "Error getting document", e);
+                        }
+                    });
+        }
     }
 
 
